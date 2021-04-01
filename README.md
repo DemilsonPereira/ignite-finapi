d92c76a1cc9df05f3bb7aea9bc8ced6486014ae6
Demilson Pereira <demilson.d2102@gmail.com>
2021-03-31 21:33:45 -0400

    Requisito 1, 2 / Regras de negócio 1, 2 feito.

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 ### Requisitos
 
-- [] Deve ser possível criar uma conta
-- [] Deve ser possível buscar o extrato bancário do cliente
+- [x] Deve ser possível criar uma conta
+- [x] Deve ser possível buscar o extrato bancário do cliente
 - [] Deve ser possível realizar um depósito
 - [] Deve ser possível realizar um saque
 - [] Deve ser possível buscar o extrato bancário do cliente por data
@@ -14,8 +14,8 @@
 
 ## Regras de negócio
 
-- [] não deve ser possível cadastrar uma conta com CPF já existente
-- [] não deve ser possível buscar extrato em uma conta não existente
+- [x] não deve ser possível cadastrar uma conta com CPF já existente
+- [x] não deve ser possível buscar extrato em uma conta não existente
 - [] não deve ser possível fazer depósito em uma conta não existente
 - [] não deve ser possível fazer saque em uma conta não existente
 - [] não deve ser possível fazer saque quando o saldo for insuficiente
--- src/index.js
+++ src/index.js
@@ -1,9 +1,47 @@
-const express = require('express');
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
+
+app.use(express.json());
+
 const PORT = 3333;
 
+const customers = [];
+
+app.post("/account", (req, res) => {
+  const { cpf, name } = req.body;
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return res.status(400).json({ message: "Customer already exists!" });
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: [],
+  });
+
+  return res.status(201).send();
+});
+
+app.get('/statement/:cpf', (req, res) => {
+    const { cpf } = req.params;
+
+    const customer = customers.find(customer => customer.cpf === cpf)
+
+    if(!customer){
+        return res.status(400).json({ message: "Customer not found"})
+    }
+
+    return res.json(customer.statement);
+});
 
 app.listen(PORT, () => {
-    console.log(`Rodando em http://localhost:${PORT}`);
+  console.log(`Rodando em http://localhost:${PORT}`);
 });
