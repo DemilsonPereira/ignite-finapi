7d4e36b8f3c4a7d6de057acc50b53cdc99992490
Demilson Pereira <demilson.d2102@gmail.com>
2021-03-31 22:15:40 -0400

    Requisito 4 / Regras de negócio 4, 5 feito.

--- src/index.js
+++ src/index.js
@@ -24,6 +24,17 @@ function verifyIfExistsAccountCPF(req, res, next) {
   return next();
 }
 
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === "credit") {
+      return acc + operation.amount;
+    }
+    return acc - operation.amount;
+  }, 0);
+
+  return balance;
+}
+
 app.post("/account", (req, res) => {
   const { cpf, name } = req.body;
 
@@ -52,20 +63,42 @@ app.get("/statement", verifyIfExistsAccountCPF, (req, res) => {
 });
 
 app.post("/deposit", verifyIfExistsAccountCPF, (req, res) => {
-    const { description, amount } = req.body;
+  const { description, amount } = req.body;
 
-    const { customer } = req;
+  const { customer } = req;
 
-    const statementOperation = {
-        description,
-        amount,
-        created_at: new Date(),
-        type: 'Credit'
-    }
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit",
+  };
 
-    customer.statement.push(statementOperation);
+  customer.statement.push(statementOperation);
 
-    return res.status(201).send();
+  return res.status(201).send();
+});
+
+app.post("/withdraw", verifyIfExistsAccountCPF, (req, res) => {
+  const { amount } = req.body;
+  const { customer } = req;
+
+  const balance = getBalance(customer.statement);
+
+  if(balance < amount){
+      return res.status(400).json({ message: "Insufficient funds!"})
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: "debit",
+  };
+
+  customer.statement.push(statementOperation);
+
+  return res.status(201).send();
+
 });
 
 app.listen(PORT, () => {
