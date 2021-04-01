8e55452fa67aa6dbd5a167a2f755b6b474806264
Demilson Pereira <demilson.d2102@gmail.com>
2021-03-31 22:34:19 -0400

    Requisito 5 feito.

--- src/index.js
+++ src/index.js
@@ -101,6 +101,17 @@ app.post("/withdraw", verifyIfExistsAccountCPF, (req, res) => {
 
 });
 
+app.get("/statement/date", verifyIfExistsAccountCPF, (req, res) => {
+    const { customer } = req;
+    const { date } = req.query;
+
+    const dateFormat = new Date(date + " 00:00");
+
+    const statement = customer.statement.filter((statement) => statement.created_at.toDateString() === new Date(dateFormat).toDateString());
+  
+    return res.json(statement);
+  });
+
 app.listen(PORT, () => {
   console.log(`Rodando em http://localhost:${PORT}`);
 });
